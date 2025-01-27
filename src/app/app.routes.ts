9a3fca0b631b9539a5c22b9fdb9b483012162da8
Jose <jamm.webdev@gmail.com>
2025-01-27 10:22:04 -0300

    adding some routes to the angular router navbar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,13 @@
 import { Routes } from '@angular/router';
 import { HomePage } from './pages/home/home.component';
 import { SchedulePage } from './pages/schedule/schedule.page';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
-  { path: 'inicio', component: HomePage},
-  { path: 'agenda', component: SchedulePage},
-  { path: '', redirectTo: '/inicio', pathMatch: 'full'}
+  { path: 'inicio', component: HomePage },
+  { path: 'agenda', component: SchedulePage },
+  { path: 'login', component: LoginComponent },
+  { path: 'subscripcion', component: RegisterComponent },
+  { path: '', redirectTo: '/inicio', pathMatch: 'full' },
 ];
