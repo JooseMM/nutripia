65c3ad9679a9ea46ab1b7c9f54a8078609416afc
Jose <jamm.webdev@gmail.com>
2025-01-26 21:46:28 -0300

    finish cta section for all views

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -3,10 +3,17 @@ import { HeroComponent } from './sections/hero/hero.component';
 import { AboutComponent } from './sections/about/about.component';
 import { WhyUsComponent } from './sections/why-us/why-us.component';
 import { PricesComponent } from './sections/prices/prices.component';
+import { CtaComponent } from './sections/cta/cta.component';
 
 @Component({
   selector: 'nt-home',
-  imports: [HeroComponent, WhyUsComponent, AboutComponent, PricesComponent],
+  imports: [
+    HeroComponent,
+    WhyUsComponent,
+    AboutComponent,
+    PricesComponent,
+    CtaComponent,
+  ],
   templateUrl: './home.component.html',
 })
 export class HomePage {}
---
+++ src/app/pages/home/sections/cta/cta.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { ButtonComponent } from '../../../../shared/button/button.component';
+
+@Component({
+  selector: 'nt-cta',
+  imports: [ButtonComponent],
+  templateUrl: './cta.component.html',
+})
+export class CtaComponent {}
