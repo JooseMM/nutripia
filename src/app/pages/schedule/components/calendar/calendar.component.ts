fcfeed91d4d7bd17196d254ca3ff70a4b4a2b011
Jose <jamm.webdev@gmail.com>
2025-01-27 10:11:56 -0300

    adding design images and new components in schedule page

---
+++ src/app/pages/schedule/components/calendar/calendar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nt-calendar',
+  imports: [],
+  templateUrl: './calendar.component.html'
+})
+export class CalendarComponent {
+
+}
