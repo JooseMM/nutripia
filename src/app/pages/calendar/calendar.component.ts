208ab5db7955e6156e907218e8e6e81d26ccabea
Jose <jamm.webdev@gmail.com>
2025-02-17 01:25:14 -0300

    moving daysOfTheMonth function to calendat components since its the only component that use it

--- src/app/pages/calendar/calendar.component.ts
+++ src/app/pages/calendar/calendar.component.ts
@@ -12,6 +12,7 @@ import {
 import { AppoitmentService } from 'src/app/shared/services/appoitments/appoitments.service';
 import DayObject from 'src/app/shared/services/appoitments/IDayObject';
 import { NgClass } from '@angular/common';
+import Appointment from 'src/models/IAppointment';
 @Component({
   selector: 'nt-calendar',
   imports: [NgClass],
@@ -22,14 +23,22 @@ export class CalendarComponent {
   appointmentService = inject(AppoitmentService);
   threeLetterWeekDay = THREE_LETTER_WEEKDAY;
   singleLetterWeekDay = SINGLE_LETTER_WEEK_DAY;
-  daysOfTheMonth: Signal<DayObject[]> = computed(() =>
-    this.appointmentService.getDaysOfTheMonth(),
-  );
-  selectedDay: Signal<Date> = computed(() =>
+  selectedDate: Signal<Date> = computed(() =>
     this.appointmentService.getSelectedDate(),
   );
+  protected daysOfTheMonth: Signal<DayObject[]> = computed(() =>
+    this.getDaysInCurrentMonth(
+      this.selectedDate().getFullYear(),
+      this.selectedDate().getMonth(),
+      this.appointmentService.getAppointmentsByMonth(
+        this.appointmentService.getAppointments(),
+        this.selectedDate().getMonth(),
+      ),
+      this.selectedDate().getDate(),
+    ),
+  );
   getMonthName: Signal<string> = computed(() =>
-    this.appointmentService.getMonthNameOf(this.selectedDay().getMonth()),
+    this.appointmentService.getMonthNameOf(this.selectedDate().getMonth()),
   );
   updateMonth(updateBy: number) {
     this.appointmentService.updateMonth(updateBy);
@@ -56,5 +65,104 @@ export class CalendarComponent {
   }
   newDaySelected(day: number) {
     this.appointmentService.updateSelectedDay(day);
+  }
+  private getDaysInCurrentMonth(
+    year: number,
+    month: number,
+    appointments: Appointment[],
+    selectedDay: number,
+  ): DayObject[] {
+    /*
+     * getting info from external api will be needed
+     *
+     * the 0 in Date objects represent the last day
+     * of the previous month, this returns the amount of days.
+     * months in js start at 0, so saying the 0 day in month 1
+     * equals to the last day in january
+     **/
+    const lastDayOfSelectedDays = new Date(year, month + 1, 0).getDate();
+    const firstDayOfSelectedMonth = new Date(year, month, 1);
+    const lastDayOfPreviousMonth = new Date(year, month, 0).getDate();
+    /*
+     * get the day of the week from sunday (0) to Saturday (6)
+     * because our format starts with monday(0) to Sunday(6) we need to
+     * add one to every given value, but to ensure this does not go beyond 6
+     * we add 6 to the value then we find the reminder when dive by 7
+     * causing the result to always wrap around 6
+     */
+    const monthStartAtDay = (firstDayOfSelectedMonth.getDay() + 6) % 7;
+    const previousMonthDays = Array.from(
+      { length: monthStartAtDay },
+      (_, index): DayObject => ({
+        isSelected: false,
+        appointments: [],
+        isDisabled: true,
+        numberDay: lastDayOfPreviousMonth - (monthStartAtDay - 1 - index),
+        /*
+         * to accurately show the last days of the month
+         * we need to substract one from the amount of day that
+         * belongs to the previous month array length (monthStartAtDay - 1),
+         * substract the current index (monthStartAtDay - 1 - index)
+         * to later substract that value to the last day of the previous month
+         * */
+      }),
+    );
+    /*
+     * create and return an array with the same length as
+     * the days of the selected month that hold an DayObject
+     * */
+    const selectedMonthDays = Array.from(
+      { length: lastDayOfSelectedDays },
+      (_, index) => {
+        const numberDay = index + 1;
+        return {
+          isSelected: selectedDay === numberDay,
+          numberDay: numberDay,
+          appointments: appointments.filter((appointments: Appointment) => {
+            return (appointments.date as Date).getDate() === numberDay;
+          }),
+          isDisabled: appointments.length > 8,
+          /*
+           * if there is more than 8 appointments in a day
+           * new appointments will not be allowed
+           */
+        } as DayObject;
+      },
+    );
+    /*
+     * in order to not have  a blank space at the end of the calendar in some cases
+     * we need to show the next month days in the available boxes left
+     *
+     * first we check what's out array length so far
+     * */
+    const combinedArraysLength =
+      previousMonthDays.length + selectedMonthDays.length;
+    /*
+     * why 35? If the calendar needs to fill days in the fith row
+     * the length so far will be less than that
+     *
+     * if its greater than 35 it means that it needs to fill days in the sixth row
+     *
+     * below we substrack to 35 and 42 to the values of the combined array
+     * this gives us the amount of days left to fill
+     * */
+    const nextMonthDaysLength =
+      combinedArraysLength < 35
+        ? 35 - combinedArraysLength
+        : 42 - combinedArraysLength;
+
+    const nextMonthDays = Array.from(
+      { length: nextMonthDaysLength },
+      (_, index): DayObject => ({
+        isSelected: false,
+        isDisabled: true,
+        numberDay: index + 1,
+        appointments: [],
+        /*
+         * then we create an array of DayObject and combined it with the rest
+         * */
+      }),
+    );
+    return [...previousMonthDays, ...selectedMonthDays, ...nextMonthDays];
   }
 }
--- src/app/shared/services/appoitments/appoitments.service.ts
+++ src/app/shared/services/appoitments/appoitments.service.ts
@@ -35,17 +35,6 @@ export class AppoitmentService {
   private authenticationState = signal(
     this.AuthService.getAuthenticationState(),
   );
-  private daysOfTheMonth: Signal<DayObject[]> = computed(() =>
-    this.getDaysInCurrentMonth(
-      this.selectedDate().getFullYear(),
-      this.selectedDate().getMonth(),
-      this.getAppointmentsByMonth(
-        this.appointments(),
-        this.selectedDate().getMonth(),
-      ),
-      this.selectedDate().getDate(),
-    ),
-  );
 
   getOnEditAppointment() {
     return this.onEditAppoitment();
@@ -62,9 +51,6 @@ export class AppoitmentService {
   }
   getSelectedDate(): Date {
     return this.selectedDate();
-  }
-  getDaysOfTheMonth(): DayObject[] {
-    return this.daysOfTheMonth();
   }
   getAppointments(): Appointment[] {
     return this.appointments();
@@ -224,7 +210,7 @@ export class AppoitmentService {
       0,
     );
   }
-  private getAppointmentsByMonth(
+  getAppointmentsByMonth(
     appointments: Appointment[],
     monthMatch: number,
   ): Appointment[] {
@@ -232,104 +218,5 @@ export class AppoitmentService {
       (appointments: Appointment) =>
         (appointments.date as Date).getMonth() === monthMatch,
     );
-  }
-  private getDaysInCurrentMonth(
-    year: number,
-    month: number,
-    appointments: Appointment[],
-    selectedDay: number,
-  ): DayObject[] {
-    /*
-     * getting info from external api will be needed
-     *
-     * the 0 in Date objects represent the last day
-     * of the previous month, this returns the amount of days.
-     * months in js start at 0, so saying the 0 day in month 1
-     * equals to the last day in january
-     **/
-    const lastDayOfSelectedDays = new Date(year, month + 1, 0).getDate();
-    const firstDayOfSelectedMonth = new Date(year, month, 1);
-    const lastDayOfPreviousMonth = new Date(year, month, 0).getDate();
-    /*
-     * get the day of the week from sunday (0) to Saturday (6)
-     * because our format starts with monday(0) to Sunday(6) we need to
-     * add one to every given value, but to ensure this does not go beyond 6
-     * we add 6 to the value then we find the reminder when dive by 7
-     * causing the result to always wrap around 6
-     */
-    const monthStartAtDay = (firstDayOfSelectedMonth.getDay() + 6) % 7;
-    const previousMonthDays = Array.from(
-      { length: monthStartAtDay },
-      (_, index): DayObject => ({
-        isSelected: false,
-        appointments: [],
-        isDisabled: true,
-        numberDay: lastDayOfPreviousMonth - (monthStartAtDay - 1 - index),
-        /*
-         * to accurately show the last days of the month
-         * we need to substract one from the amount of day that
-         * belongs to the previous month array length (monthStartAtDay - 1),
-         * substract the current index (monthStartAtDay - 1 - index)
-         * to later substract that value to the last day of the previous month
-         * */
-      }),
-    );
-    /*
-     * create and return an array with the same length as
-     * the days of the selected month that hold an DayObject
-     * */
-    const selectedMonthDays = Array.from(
-      { length: lastDayOfSelectedDays },
-      (_, index) => {
-        const numberDay = index + 1;
-        return {
-          isSelected: selectedDay === numberDay,
-          numberDay: numberDay,
-          appointments: appointments.filter((appointments: Appointment) => {
-            return (appointments.date as Date).getDate() === numberDay;
-          }),
-          isDisabled: appointments.length > 8,
-          /*
-           * if there is more than 8 appointments in a day
-           * new appointments will not be allowed
-           */
-        } as DayObject;
-      },
-    );
-    /*
-     * in order to not have  a blank space at the end of the calendar in some cases
-     * we need to show the next month days in the available boxes left
-     *
-     * first we check what's out array length so far
-     * */
-    const combinedArraysLength =
-      previousMonthDays.length + selectedMonthDays.length;
-    /*
-     * why 35? If the calendar needs to fill days in the fith row
-     * the length so far will be less than that
-     *
-     * if its greater than 35 it means that it needs to fill days in the sixth row
-     *
-     * below we substrack to 35 and 42 to the values of the combined array
-     * this gives us the amount of days left to fill
-     * */
-    const nextMonthDaysLength =
-      combinedArraysLength < 35
-        ? 35 - combinedArraysLength
-        : 42 - combinedArraysLength;
-
-    const nextMonthDays = Array.from(
-      { length: nextMonthDaysLength },
-      (_, index): DayObject => ({
-        isSelected: false,
-        isDisabled: true,
-        numberDay: index + 1,
-        appointments: [],
-        /*
-         * then we create an array of DayObject and combined it with the rest
-         * */
-      }),
-    );
-    return [...previousMonthDays, ...selectedMonthDays, ...nextMonthDays];
   }
 }
