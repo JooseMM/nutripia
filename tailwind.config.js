51e71529ac69ed5e0a57dbe4de60b637fb6af0cf
Jose <jamm.webdev@gmail.com>
2025-02-26 02:53:28 -0300

    optimice images to webp

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,8 +31,8 @@ module.exports = {
       backgroundImage: {
         "check-icon": "url('assets/icons/check-icon.svg')",
         "uncheck-icon": "url('assets/icons/uncheck-icon.svg')",
-        "login-image": "url('assets/images/login-bg.png')",
-        "register-image": "url('assets/images/register-bg.png')",
+        "login-image": "url('assets/images/login-bg.webp')",
+        "register-image": "url('assets/images/register-bg.webp')",
       },
     },
   },
